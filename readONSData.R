43231413725dc8015a31e7c7d94331ef6f5267f3
Francisco Marco-Serrano <support@matterof.biz>
2014-11-03 21:13:09 +0000

    0.1
    First draft; discovery and read functions

--- readONSData.R
+++ readONSData.R
@@ -19,22 +19,42 @@ readONSapikey <- function(){
 }
 
 
+## Function to discover data
+discoverONSData <- function(){
+  
+  Domain = "http://data.ons.gov.uk/ons/api/data/"  # Beginning of the url
+  ApiKey = readONSapikey()                         # API Key, as provided by ONS
+  
+  url <- paste(Domain,
+               "?apikey=", ApiKey,
+               sep = "")
+
+  doc <- xmlParse(url)
+  doc_data <- xmlToList(doc)
+  doc_data
+  
+  #TODO: present list in a human readable way.
+  
+}
+
+  
 ## Function to retrieve data
-readONSData <- function(Path="QS201EW",
-                        QueryContext="Census",
-                        QueryGeography="2011PCONH"){
+readONSData <- function(Path="",
+                        QueryContext="",
+                        QueryGeography=""){
 
-	Domain = "http://data.ons.gov.uk/ons/api"	# Beginning of the url
-	Context = "/data/dataset/"      				  # Request dataset
-	Path = Path					                      #
-	Representation = "xml"	                  # Type of file (xml, json, csv...)
-	ApiKey = readONSapikey()	                # API Key, as provided by ONS
-	QueryContext = QueryContext               # Census, Socio-Economic, Economic, Social
-	QueryGeography = QueryGeography           # Geographic filter (?geog=<hierarchy name>)
-	QuerySlicing = ""					                # Dimension filter (?dm=/<dimension>=<value>)
-	QueryPaging = ""					                # Paging filter
+	Domain = "http://data.ons.gov.uk/ons/api/data/"	# Beginning of the url
+	Path = Path					                            #
+	Representation = "xml"	                        # Type of file (xml, json, csv...)
+	ApiKey = readONSapikey()	                      # API Key, as provided by ONS
+	QueryContext = QueryContext                     # Census, Socio-Economic, Economic, Social
+	QueryGeography = QueryGeography                 # Geographic filter (?geog=<hierarchy name>)
+	QuerySlicing = ""					                      # Dimension filter (?dm=/<dimension>=<value>)
+	QueryPaging = ""					                      # Paging filter
 
-
+  # The API's URLs consists of the following parts:
+  # http://[domain]/[context root]/[resource].[representation]?[query string]
+  
   # TODO: implement slicing and paging
 	url <- paste(Domain, Context, Path, "/set.", Representation,
 			   "?context=", QueryContext,
@@ -42,12 +62,9 @@ readONSData <- function(Path="QS201EW",
 			   "&apikey=", ApiKey,
 			   sep = "")
 
-	print(url)						                    # Only for debugging
-
   require(rsdmx)
 
   # TODO: control for errors (i.e. too many rows retrieved)
   doc <- readSDMX(url)
-  doc
   
 }
